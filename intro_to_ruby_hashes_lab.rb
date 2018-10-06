85d4571cc457b189291cc4e212d86e84c6d8f654
Brandon Teich <brandonteich123@gmail.com>
2018-10-06 01:02:54 +0000

    automatically backed up by learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -13,7 +13,7 @@ end
 
 def monopoly_with_second_tier
 
-		monopoly = {:railroads => "Dwayne The Rock Johnson"}
+		monopoly = {:railroads => "railroad", :value = > {}}
 end
 
 def monopoly_with_third_tier
