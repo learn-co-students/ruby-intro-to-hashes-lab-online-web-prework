fc304c7e983796bc9bf0f1a0bfbb2450439b6ae3
Brandon Teich <brandonteich123@gmail.com>
2018-10-06 01:04:03 +0000

    automatically backed up by learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -13,7 +13,7 @@ end
 
 def monopoly_with_second_tier
 
-		monopoly = {:railroads => "railroad", :value = > {}}
+		monopoly = {:railroads => {railroad: 4, value: {}}}
 		monopoly = {railroads: {pieces: 4, names: {}, rent_in_dollars: {}}}
 end
 
