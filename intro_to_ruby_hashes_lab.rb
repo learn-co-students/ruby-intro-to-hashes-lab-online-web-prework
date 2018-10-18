9803ff9ff37f5ebe8511b5f0c595bf0727b33342
Cookie McCormick <muntananuchat@gmail.com>
2018-10-17 21:58:57 -0400

    Done.

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -1,31 +1,31 @@
 def new_hash
-	hash = {}
+  hash = {}
 end
 
 def actor
-	actor = {
-		name: "Dwayne The Rock Johnson"
+  actor = {
+	  name: "Dwayne The Rock Johnson"
   }
 end
 
 def monopoly
-	monopoly = {
-		railroads: {}
-	}
+  monopoly = {
+    railroads: {}
+  }
 end
 
 def monopoly_with_second_tier
-	monopoly = {
+  monopoly = {
 		railroads: {
 			pieces: 4,
 			names: {},
 			rent_in_dollars: {}
 		}
-	}
+  }
 end
 
 def monopoly_with_third_tier
-	monopoly = {
+  monopoly = {
 		railroads: {
 			pieces: 4,
 			names: {
@@ -41,11 +41,11 @@ def monopoly_with_third_tier
 				four_pieces_owned: 200
 			}
 		}
-	}
+  }
 end
 
 def monopoly_with_fourth_tier
-	monopoly = {
+  monopoly = {
 		railroads: {
 			pieces: 4,
 			names: {
@@ -69,123 +69,5 @@ def monopoly_with_fourth_tier
 				four_pieces_owned: 200
 			}
 		}
-	}
+  }
 end
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# def new_hash
-#   hash = {}
-# end
-
-# def actor
-#   hash = {
-# 	:name => "Dwayne The Rock Johnson"
-#   }
-# end
-
-# def monopoly
-#   monopoly = {
-# 	:railroads => {}
-#   }
-# end
-
-# def monopoly_with_second_tier
-# #copy and paste the monopoly hash you built in the previous method. We're going to add stuff to it here!
-#   monopoly = {
-#     :railroads => {
-# 	  :pieces => 4, 
-# 	  :names => {}, 
-# 	  :rent_in_dollars => {}
-# 	}
-#   }
-# end
-
-# def monopoly_with_third_tier
-# #copy and paste the monopoly hash you built in the previous method. We're going to add stuff to it here!
-#   monopoly = {
-#     :railroads => {
-# 	  :pieces => 4, 
-# 	  :names => {
-# 		:reading_railroad => {},
-# 		:pennsylvania_railroad => {},
-# 		:b_and_o_railroad => {},
-# 		:shortline => {}
-# 	  }, 
-# 	  :rent_in_dollars => {
-# 		:one_piece_owned => 25,
-# 		:two_pieces_owned => 50,
-# 		:three_pieces_owned => 100,
-# 		:four_pieces_owned => 200
-# 	  }
-# 	}
-#   }	
-# end
-
-# def monopoly_with_fourth_tier
-# #copy and past the monopoly hash you build in the previous method. We're going to add stuff to it here!
-#   monopoly = {
-#     :railroads => {
-# 	  :pieces => 4, 
-# 	  :names => {
-# 		:reading_railroad => { 
-# 		  :mortgage_value.to_s => "$100"
-# 		},
-# 		:pennsylvania_railroad => {
-# 		  :mortgage_value.to_s => "$200"
-# 		},
-# 		:b_and_o_railroad => {
-# 		  :mortgage_value.to_s => "$400"
-# 		},
-# 		:shortline => {
-# 		  :mortgage_value.to_s => "$800"
-# 		}
-# 	  }, 
-# 	  :rent_in_dollars => {
-# 		:one_piece_owned => 25,
-# 		:two_pieces_owned => 50,
-# 		:three_pieces_owned => 100,
-# 		:four_pieces_owned => 200
-# 	  }
-# 	}
-#   }	
-# end
-
-
-
-
